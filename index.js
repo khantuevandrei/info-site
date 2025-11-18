38b87f527b51509ef10dab59b07a357bac02bf8e
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-18 09:25:44 +0500

    Refactor to use express

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
-import http from "node:http";
-import fs from "node:fs";
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/html" });
-  const requestedURL = req.url;
-  let path = "./";
-
-  switch (requestedURL) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact":
-      path += "contact.html";
-      break;
-    default:
-      path += "404.html";
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.end();
-      return;
-    }
-    res.end(data);
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-server.listen(8000);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, (error) => {
+  if (error) throw error;
+  console.log(`Express app listening on port ${PORT}`);
+});
